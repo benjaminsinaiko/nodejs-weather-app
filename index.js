97959a1db5f8c52c17714439c38bf0e2bd9b058b
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-19 00:02:03 -0500

    Create api call, with weather result

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 require('dotenv/config');
-console.log(process.env.SECRET_MESSAGE);
+
+const request = require('request');
+
+let apiKey = process.env.API_KEY;
+let city = 'chicago';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+request(url, function(err, response, body) {
+  if (err) {
+    console.log(apiKey);
+    console.log('error: ', error);
+  } else {
+    console.log('body: ', body);
+  }
+});
