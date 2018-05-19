7256e3858ec98dcbbc8ce5d7712e035109df2c5a
benjaminsinaiko <ben.jamin@outlook.com>
2018-05-19 01:58:18 -0500

    Add public folder with css

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(3000, function() {
